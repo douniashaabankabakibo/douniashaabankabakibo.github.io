4fc02c0fe187b8f7d03786d0b926ea5af703d1be
douniashaabankabakibo <135051394+douniashaabankabakibo@users.noreply.github.com>
2026-04-13 05:14:57 +0000

    Deploying to gh-pages from @ douniashaabankabakibo/douniashaabankabakibo.github.io@200f1c2903f06aab98293316d131f85d2a2cc236 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-art",
-          title: "Art",
+          title: "art",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {

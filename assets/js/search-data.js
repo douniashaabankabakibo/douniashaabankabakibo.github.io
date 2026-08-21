f0c1eb511d5f18e00a2ffa2531f18a2517ad0df8
douniashaabankabakibo <135051394+douniashaabankabakibo@users.noreply.github.com>
2026-08-21 22:48:35 +0000

    Deploying to gh-pages from @ douniashaabankabakibo/douniashaabankabakibo.github.io@5975a11cba09cd8fa7e154ae7a854af17df8af0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "PhD Candidate in Physics at Université de Montréal, working at the intersection of machine learning and condensed matter physics.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
